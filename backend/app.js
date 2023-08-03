9187f5dc92d9297cff6df63bb2dcc4045a3dca6d
iotunipamplona <iotunipamplona@gmail.com>
2023-08-03 02:19:45 -0500

    er
    t

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,35 @@ const dbconexion = require('./config/mongo');
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
-app.use(express.json());
+dbconexion();
 
+app.use(cors());
+
+app.use(express.json());
+app.use("/api", require("./routes"));
 app.get("/", (req, res) => {
-  res.send("<h1>Bienvenido a la Aplicación Express en Vercel</h1>");
+  const htmlResponse = `
+  <html>
+      <head>
+          <title> PAGINA EN VERCEL</title>
+      </head>
+      <body>
+          <h1>El proyecto en vercelll</h1>
+      </body>
+  </html>`;
+  res.send(htmlResponse);
 });
 
-app.use("/api", require("./routes"));
 
-dbconexion();
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  });
+});
+
+
+
+
+
 
 module.exports = app;
